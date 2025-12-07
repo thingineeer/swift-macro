55a4548ac21fa0b08295b65075a67c1ae042d3ea
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-12-07 20:01:27 +0900

    [Setting] 프로젝트 코드 베이스 앱 초기 세팅

---
+++ Swift_Macro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Macro
+//
+//  Created by 이명진 on 12/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
